89606b434523111f97c3c9f904351d689b58a022
lam300695 <lam300695@gmail.com>
2019-11-19 22:48:18 +0700

    Kiem tra

--- customer-manager-ORM-master/src/main/java/com/codegym/cms/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.cms.repository;
-
-import com.codegym.cms.model.Customer;
-import com.codegym.cms.model.Deparment;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
-    Iterable<Customer> findAllByDeparment(Deparment deparment);
-}
--- customer-manager-ORM-master/src/main/java/com/codegym/cms/repository/DeparmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.cms.repository;
-
-import com.codegym.cms.model.Deparment;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeparmentRepository extends PagingAndSortingRepository<Deparment, Long> {
-}
